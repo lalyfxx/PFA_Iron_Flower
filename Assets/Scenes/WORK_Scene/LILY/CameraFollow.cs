808603363a739567672d1ffe9930ec9abfa5fbc6
lalyfxx <125013395+lalyfxx@users.noreply.github.com>
2025-04-25 13:35:49 +0200

    scene_changement

--- Assets/Scenes/WORK_Scene/LILY/CameraFollow.cs
+++ Assets/Scenes/WORK_Scene/LILY/CameraFollow.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target; // Ta boule
-    public Vector3 offset = new Vector3(0, 5, -10); // Distance caméra-boule
+    public Transform target; 
+    public Vector3 offset = new Vector3(0, 5, -10); 
 
     void LateUpdate()
     {

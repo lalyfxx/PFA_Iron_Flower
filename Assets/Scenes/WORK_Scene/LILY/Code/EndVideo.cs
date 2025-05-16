5d88788f860463694cbf8b12426f2c924d0a49f6
lalyfxx <125013395+lalyfxx@users.noreply.github.com>
2025-05-17 01:47:12 +0200

    VIDEOOOO

---
+++ Assets/Scenes/WORK_Scene/LILY/Code/EndVideo.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class EndVideo : MonoBehaviour
+{
+    public int sceneIndexToLoad = 2; 
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        if (videoPlayer != null)
+        {
+            
+            videoPlayer.loopPointReached += OnVideoEnd;
+        }
+        else
+        {
+            Debug.LogError("Aucun VideoPlayer trouvé sur cet objet.");
+        }
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(sceneIndexToLoad);
+    }
+}
+

b7852d5a4b03ef09d8fb918b2cd32d1bed76266e
Missowy <125024457+Missowy@users.noreply.github.com>
2025-05-21 21:24:24 +0200

    ça fontionne a moitié... je regarderais demain

--- Assets/Scenes/WORK_Scene/NOEMY/CODE/Checkpoint.cs
+++ Assets/Scenes/WORK_Scene/NOEMY/CODE/Checkpoint.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
- 
+
 public class Checkpoint : MonoBehaviour
 {
     public BoxCollider2D trigger;
- 
+    public Animator _animCheckpoint;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.attachedRigidbody.CompareTag("Player"))
@@ -11,5 +12,19 @@ public class Checkpoint : MonoBehaviour
             RespawnController.Instance.respawnPoint = transform;
             trigger.enabled = false;
         }
+
+        if (collision.attachedRigidbody.CompareTag("Player"))
+        {
+            _animCheckpoint.Play("Iron_Flower_Aseprite_FINAL_MINI_Chek_Point_Animation_0");
+        }
+        else
+        {
+            _animCheckpoint.SetBool("isTouched", false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _animCheckpoint.SetBool("isTouched", false);
     }
 }

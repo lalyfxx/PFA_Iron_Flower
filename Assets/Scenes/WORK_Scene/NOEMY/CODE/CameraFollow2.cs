d1150321d38416f4c4097eb2bbbd8bf7b3034258
lalyfxx <125013395+lalyfxx@users.noreply.github.com>
2025-05-21 15:04:58 +0200

    camera_update

--- Assets/Scenes/WORK_Scene/NOEMY/CODE/CameraFollow2.cs
+++ Assets/Scenes/WORK_Scene/NOEMY/CODE/CameraFollow2.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraFollow2 : MonoBehaviour
 {
     public float defaultZoom = 5f;
+    public Transform target;
+    public Vector3 offset = new Vector3(0f, 0f, -10f); // Pour caméra 2D, Z = -10 est standard
+    public float smoothSpeed = 5f;
     public float targetZoom = 5f;
 
     public HashSet<CameraZoomTrigger> triggerBoxes = new();
@@ -28,6 +31,13 @@ public class CameraFollow2 : MonoBehaviour
 
     void Update()
     {
+        if (target != null)
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+
         var currentTriggerBox = triggerBoxes
             .OrderBy(t => t.zoomPriority)
             .LastOrDefault();

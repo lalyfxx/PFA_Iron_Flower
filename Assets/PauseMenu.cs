29ae5ffef6b9753325aeadaae96346d86ccd0f65
Missowy <125024457+Missowy@users.noreply.github.com>
2025-05-25 15:25:51 +0200

    menu_pause_qui_fonctionne

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,29 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1;
+    }
+}
